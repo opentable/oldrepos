1f3731e32be6edbed35b9c966394b3b7be30e7a4
Judson <nyarly@gmail.com>
2018-07-27 10:53:54 -0700

    Updated current_proposals, and monitoring script

--- monitor_proposals.rb
+++ monitor_proposals.rb
@@ -16,11 +16,19 @@ def review_proposals(token)
 
   client = Octokit::Client.new(access_token: token)
 
+  active = nil
   File.open("current_proposals", "r") do |file|
-    file.each_line do |line|
+    active = file.each_line.select do |line|
       review(line.chomp, client)
     end
   end
+  File.open("still_active", "w") do |file|
+    active.each do |reponame|
+      file.write reponame
+    end
+  end
+  puts "Wrote 'still_active'"
+  puts %x[diff current_proposals still_active]
 end
 
 def review(reponame, client)
@@ -30,9 +38,11 @@ def review(reponame, client)
     c = client.user(c.login)
     pp [c.login, c.name, c.email]
   end
+  return true
 
-rescue Octokit::InvalidRepository
+rescue Octokit::InvalidRepository, Octokit::NotFound
   puts "Good news: #{reponame} is no longer a valid repo!"
+  return false
 rescue => ex
   p ex
 end
